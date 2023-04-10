e6d58437256c6d57fdcb9ce6e675f4c6942a00f9
dennisjanvogt <dennisjanvogt@gmail.com>
2023-04-10 23:57:07 +0200

    added Helper Class

---
+++ src/Helpers.java
@@ -0,0 +1,13 @@
+public class Helpers {
+    public static void clearConsole() {
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                Runtime.getRuntime().exec("clear");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
